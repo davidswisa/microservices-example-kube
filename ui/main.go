45d95b533a49d5179b69260224d60bfedfc5200b
swisa <d.swisa@f5.com>
2022-06-30 16:00:13 +0000

    fix: initial commit

--- ui/main.go
+++ ui/main.go
@@ -17,6 +17,26 @@ func gui() func(http.ResponseWriter, *http.Request) {
 	})
 }
 
+func reservations() func(http.ResponseWriter, *http.Request) {
+	return http.HandlerFunc(func(wrt http.ResponseWriter, req *http.Request) {
+		switch req.Method {
+		case "GET":
+			fmt.Println("Time to wake up!")
+		case "POST":
+			fmt.Println("Time to go to bed.")
+		case "PUT":
+			fmt.Println("Time to wake up!")
+		case "DELETE":
+			fmt.Println("Time to go to bed.")
+		default:
+			fmt.Println("error")
+		}
+		// fmt.Println(req.Method)
+		content := "{}"
+		wrt.Write([]byte(content))
+	})
+}
+
 func imagesHandler(w http.ResponseWriter, r *http.Request) {
 	http.ServeFile(w, r, "./images/restaurant-circular.svg")
 }
@@ -29,10 +49,11 @@ func main() {
 
 	// Add handle func for producer.
 	http.HandleFunc("/", gui())
+	http.HandleFunc("/reservations", reservations())
 	http.HandleFunc("/images/restaurant-circular.svg", imagesHandler)
 	http.HandleFunc("/favicon.ico", faviconHandler)
 
 	// Run the web server.
-	fmt.Println("starting producer-api...")
+	fmt.Println("starting web server...")
 	log.Fatal(http.ListenAndServe(":8084", nil))
 }
